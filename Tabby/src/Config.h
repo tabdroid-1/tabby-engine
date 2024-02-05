5930d3541831ecc0ae48f97351ce448cfa932853
tabdroid-1 <tabdroid.official@gmail.com>
2024-02-05 02:07:09 +0100

    Using SDL2 instead of glfw

--- Tabby/src/Config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define MAX_DEVICES 1
-#define MAX_SOUNDS 32
--- Tabby/src/Tabby/Renderer/RenderCommand.cpp
+++
@@ -1,8 +0,0 @@
-#include "Tabby/Renderer/RenderCommand.h"
-#include "tbpch.h"
-
-namespace Tabby {
-
-Scope<RendererAPI> RenderCommand::s_RendererAPI = RendererAPI::Create();
-
-}
