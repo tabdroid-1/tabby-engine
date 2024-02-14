ce381862d99a6f0ab3ae0d1dc506f915cb4d1684
tabby <tabby@tabbys-air.home>
2024-02-14 03:09:40 +0100

    Android support

--- android/app/.cxx/Debug/1q351a4n/arm64-v8a/Tabby/vendor/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tabby/Projects/C++/TabbyEngine/Tabby/vendor/yaml-cpp
-# Build directory: /Users/tabby/Projects/C++/TabbyEngine/android/app/.cxx/Debug/1q351a4n/arm64-v8a/Tabby/vendor/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
